8cba377108904974899ca55b6275433e0252a53a
Federico Braidi <289937@studenti.unimore.it>
2025-01-12 00:42:44 +0100

    started writing structure of code in file prova.py

--- prova.py
+++ prova.py
@@ -1,95 +1,62 @@
 import random
 import numpy as np
 
-class PerudoGame:
-    def __init__(self, num_players=2, num_dice_per_player=5):
-        self.num_players = num_players
-        self.num_dice_per_player = num_dice_per_player
-        self.dice = {player: [random.randint(1, 6) for _ in range(num_dice_per_player)] 
-                     for player in range(num_players)}
-        self.current_bid = None  # Format: (quantity, face)
-        self.active_player = 0
+class InfoSet():
+    
+    def __init__():
+        
+        
+    
+class InfosetStore():
+    
+    def __init__():
+        
+        
+    
+    def compute_bounds(b1,b2):
+        
+        
+    
+class GameState():
+    
+    
 
-    def roll_dice(self):
-        for player in range(self.num_players):
-            self.dice[player] = [random.randint(1, 6) for _ in range(len(self.dice[player]))]
-
-    def is_valid_bid(self, quantity, face):
-        if self.current_bid is None:
-            return True
-        return (quantity > self.current_bid[0]) or (quantity == self.current_bid[0] and face > self.current_bid[1])
-
-    def make_bid(self, player, quantity, face):
-        if not self.is_valid_bid(quantity, face):
-            raise ValueError("Invalid bid!")
-        self.current_bid = (quantity, face)
-        self.active_player = (player + 1) % self.num_players
-
-    def call_bluff(self):
-        quantity, face = self.current_bid
-        actual_count = sum(dice.count(face) for dice in self.dice.values())
-        bluff = actual_count < quantity
-        return bluff
-
-
-class CounterfactualRegretMinimizer:
-    def __init__(self, num_actions):
-        self.num_actions = num_actions
-        self.regret_sum = np.zeros(num_actions)
-        self.strategy = np.zeros(num_actions)
-        self.strategy_sum = np.zeros(num_actions)
-
-    def get_strategy(self, realization_weight):
-        positive_regrets = np.maximum(self.regret_sum, 0)
-        normalizing_sum = np.sum(positive_regrets)
-        if normalizing_sum > 0:
-            self.strategy = positive_regrets / normalizing_sum
-        else:
-            self.strategy = np.ones(self.num_actions) / self.num_actions
-        self.strategy_sum += realization_weight * self.strategy
-        return self.strategy
-
-    def get_average_strategy(self):
-        normalizing_sum = np.sum(self.strategy_sum)
-        if normalizing_sum > 0:
-            return self.strategy_sum / normalizing_sum
-        return np.ones(self.num_actions) / self.num_actions
-
-    def update_regret(self, action_utilities, realization_weight):
-        for a in range(self.num_actions):
-            print(a)
-            self.regret_sum[a] += realization_weight * (action_utilities[a] - action_utilities.dot(self.strategy))
-
-
-class PerudoTrainer:
-    def __init__(self, num_players=2, num_dice=5):
-        self.game = PerudoGame(num_players, num_dice)
-        self.regret_minimizers = [CounterfactualRegretMinimizer(num_actions=2) for _ in range(num_players)]
-
-    def train(self, iterations=10000):
-        for _ in range(iterations):
-            self.game.roll_dice()
-            utilities = self.play_round()
-            for p in range(self.game.num_players):
-                self.regret_minimizers[p].update_regret(utilities[p], 1.0)
-
-    def play_round(self):
-        utilities = np.zeros(self.game.num_players)
-        for p in range(self.game.num_players):
-            strategy = self.regret_minimizers[p].get_strategy(1.0)
-            action = np.random.choice(len(strategy), p=strategy)
-            if action == 0:  # Bluff
-                utilities[p] = -1  # Penalty for being called on a bluff
-            elif action == 1:  # Truth
-                utilities[p] = 1  # Reward for truthful play
-        return utilities
-
-
-# Example usage
-if __name__ == "__main__":
-    trainer = PerudoTrainer(num_players=2, num_dice=5)
-    trainer.train(iterations=1000)
-
-    # Print average strategies
-    for player_id, rm in enumerate(trainer.regret_minimizers):
-        print(f"Player {player_id} strategy: {rm.get_average_strategy()}")
+def cfr(gamestate, current_player, t, reach1, reach2, flag, update_player):
+    
+    # Check if node (gamestate) is terminal, if it is return the payoff for update_player
+    
+    # Check if node (gamestate) is a chance node (in our case at the top of the tree), its enough to check if the gamestate has attributes p1roll and p2roll set.
+    # Do an if gs.p1roll==0 and if so define an expected value variable and iterate through all possible chance outcomes for player1. For all possible chance
+    # outcomes create a gamestate which has that outcome as p1roll, define a new chancereachprobability by multiplying the chanceprobability of current outcome 
+    # with current chancereach and recursively call cfr with cfr(new_gs, current_player, bidseq, reach1, reach2, newChanceReach, phase, updatePlayer) nothing 
+    # besides chancereach and gs have changed because no actual play has taken place 
+    
+    # Do an elseif with the same approach but for player2, both this and the previous return EV which is the sum of the recursive cfrs weighted by the chance
+    # probabilities of obtaining them
+    
+    # Check for possible pruning optimization, if flag is 1 and (current_player==update_player==1 and reach2<=0)||(current_player==update_player==2 and reach1<=0)
+    # then we set flag to 2. Then if flag is 2 and (current_player==update_player==1 and reach1<=0)||(current_player==update_player==2 and reach2<=0) we 
+    # automatically return 0
+    
+    # Create an Infoset object
+    
+if __name__=='main':
+    
+    # Create the InfoSetStore iss as it is done in line 564 of bluff.cpp
+    
+    # Create two variables, one for the number of possible chance outcomes for player 1 and one for player 2
+    
+    # Define iscWidth which is the number of bits needed to encode the chance outcomes, ceiling(log2 of the max of the two numChanceOutcomes)
+    
+    # Create a vector of size numbids where we assign in order the bids in the format (quantity*10)+face,
+    # don't forget that wildcard bids are favoured (more probable), follow function initBids() in bluff.cpp.
+    
+    # Define a number of iterations to run
+    
+    # Set the bidsequence as a long long 0 or find an equivalent way with strings
+    
+    # Create a for, that runs for the defined number of iterations and at each step for each (of the two) player(s) creates an instance of the GameState class and 
+    # runs the cfr algorithm with cfr(gamestate_i, i, 0, bidseq, 1.0, 1.0, 1, 1), gets the expected value this way
+    
+    # If we are at the last iteration (or every few iterations) compute the bounds with the iss.compute_bounds function and best reponses with function
+    # compute_best_responses, to write it take after computeBestResponse in file br.cpp
